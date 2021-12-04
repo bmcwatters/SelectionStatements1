3847659e436b7e513301dc652feb340af54f726d
bryan mcwatters <bmcwatters@comcast.net>
2021-12-04 17:21:40 -0500

    initial commit

---
+++ SelectionStatements1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+var r = new Random();
+int favNumber = 333;
+Console.WriteLine("Try to guess my favorite number");
+int userInput = int.Parse(Console.ReadLine());
+
+if (userInput < favNumber)
+{
+    Console.WriteLine("Number is too low!");
+}
+else if (userInput > favNumber)
+{
+    Console.WriteLine("Number is too high!");
+}
+else if (userInput == favNumber)
+{
+    Console.WriteLine("Perfect, way to go!");
+}
+else
+{
+    Console.WriteLine("Nevermind!");
+}
